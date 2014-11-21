254266a137daedc6abe5a12f8e20f4b602c0564d
willeponken <william@megalicious.org>
2014-11-21 12:30:59 +0100

    Added mac lookup

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,39 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('index', {
-    title: 'HTSIT Hotspot' 
+var sys = require('sys'),
+    exec = require('child_process').exec,
+    child;
+
+function getMAC(iface, ip, callback) {
+  child = exec('arping -f -I ' + iface + ' ' + ip + ' | /bin/egrep -o \[0-9A-F:\]\{17\}', function (err, stdout, stderr) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, stdout);
+    }
   });
+}
+
+
+router.get('/', function(req, res) {
+
+  getMAC('wifi0', req.ip, function(err, mac) {
+    if (err) {
+      res.render('index', {
+        title: 'HTSIT Hotspot',
+        ip: req.ip,
+        mac: ''
+      });
+    } else {
+      res.render('index', {
+        title: 'HTSIT Hotspot',
+        ip: req.ip,
+        mac: mac 
+      });
+    }
+  });
+ 
 });
 
 module.exports = router;
