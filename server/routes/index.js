3f9c8c8d55b6c17c66553aec4355bb51d4d76aa6
willeponken <william@megalicious.org>
2014-11-29 17:30:04 +0100

    Error handling for timeout etc.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,18 @@ var sys = require('sys'),
     child;
 
 function getMAC(iface, ip, callback) {
+  
+  var checkResp = setTimeout(function noResp() {
+    child.kill('SIGHUP'); // Kill the arping process
+    callback('Arping timeout for ' + ip, null);
+  }, 5000);
+  
   child = exec('arping -f -I ' + iface + ' ' + ip + ' | /bin/egrep -o \[0-9A-F:\]\{17\}', function (err, stdout, stderr) {
     if (err) {
+      clearTimeout(checkResp);
       callback(err, null);
     } else {
+      clearTimeout(checkResp);
       callback(null, stdout);
     }
   });
@@ -20,14 +28,17 @@ router.get('/', function(req, res) {
 
   getMAC('wifi0', req.ip, function(err, mac) {
     if (err) {
+      console.warn('[getMac] Warning:', err);
       res.render('index', {
         title: 'HTSIT Hotspot',
+        err: true,
         ip: req.ip,
         mac: ''
       });
     } else {
       res.render('index', {
         title: 'HTSIT Hotspot',
+        err: false,
         ip: req.ip,
         mac: mac 
       });
