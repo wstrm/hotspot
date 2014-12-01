0b3289eab3d0ca933469255c5967ff9f0ae79746
willeponken <william@megalicious.org>
2014-12-01 09:44:29 +0100

    Updated README.md and fixed some stuff with the tests

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-hotspot
+Hotspot
 =======
 
 Hotspot landing page for HTSIT
+
+## Deploy
+`npm install`
+`npm start` (You'll need access to run arping)
+
+## Test
+Before you run tests you'll need a active network connection,
+and also access to run arping.
+
+Run tests with
+`npm test`
--- test/server.js
+++ test/server.js
@@ -6,7 +6,8 @@ describe('hotspot', function() {
   describe('.getMAC(iface, ip, callback)', function() {
     it('should return callback with MAC', function(done) {
       
-      var netInfo = net.getInfo().IPv4[0];
+      // Will only work if you're connected to a network
+      var netInfo = net.getInfo().IPv4[0] || net.getInfo().IPv6[0];
       hotspot.getMAC(netInfo.interface, netInfo.gateway, function resp(err, mac) {
         assert.equal(err, null);
         assert.equal(typeof mac, 'string');
