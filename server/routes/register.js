671eb38e33cb69b829d7342c6837bb9f6133f99c
willeponken <willeponken@gmail.com>
2015-01-01 23:44:04 +0100

    Just some thoughts

--- server/routes/register.js
+++ server/routes/register.js
@@ -6,11 +6,27 @@ var express = require('express'),
 var credFactory = new hotspot.credFactory('config/hotspot.json'),
     cjdns = new CJDNS('config/cjdns.json');
 
+console.log('test');
+
 router.post('/', function(req, res) {
   var errorHandler = new hotspot.errorHandler(res);
   var userData = req.body;
+  var userAddress = req.ip.replace('::ffff:', '');
   
   console.log('[/register] New registration:', userData);
+
+  //console.log(userAddress);
+
+  // Probably need to rethink some stuff.
+  /*
+  cjdns.NodeStore_nodeForAddr('fc74:73e8:3913:f15b:d463:2fe7:db69:381e', function nodeResult(err, data) {
+    if (err) {
+      return errorHandler(err);
+    } else {
+      console.log(data);
+    }
+  });
+  */
 
   hotspot.createHash(256, function hashResult(err, hash) {
     if (err) {
